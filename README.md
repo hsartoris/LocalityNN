dbc068c9a19fb08fe18893f99d24f2c032d20cac
Hayden Sartoris <hsartoris@gmail.com>
2018-07-28 17:23:45 -0400

    Started simulator template; need to abstract. see README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LocalityNN
+
+TODO
+---
+There's a lot of code reuse between the Generator and Simulator templates.  
+Abstract the parts of functionality that are common, such as `get_params` etc, 
+into a base template, and rename the Generator and Simulator templates to 
+reflect individual status.
--- Simulator/Template.py
+++ Simulator/Template.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Dict
 
 class Template(object):
     """Template for simulator modules found in Simulator.modules.
@@ -7,5 +8,16 @@ class Template(object):
 
     `params` defines details for simulation that may be specific to a given 
     module.
+
+    """
+
+    def __init__(self, params: Dict = None) -> None:
+        self.params: Dict = self._get_default_params()
+        if not params is None:
+            self.set_params(params)
+
+    @classmethod
+    def _get_default_params(cls) -> Dict:
+        raise NotImplementedError
 
 
